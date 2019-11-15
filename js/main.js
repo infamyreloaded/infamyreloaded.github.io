64dfec238f7d1d48680580b78bf16b941ac41da2
Turner <20446954+Trnrr@users.noreply.github.com>
2019-11-14 19:08:58 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,40 @@
-$(".animate").hide().show(800);
-$(".fade").hide().fadeIn(800,"swing");
+// Get rid of particles t/f
+const doParticles = true;
+
+function getWidth() { // credit to travis on stack overflow
+  return Math.max(
+    document.body.scrollWidth,
+    document.documentElement.scrollWidth,
+    document.body.offsetWidth,
+    document.documentElement.offsetWidth,
+    document.documentElement.clientWidth
+  );
+}
+if (doParticles) {
+	if (getWidth() < 400) $.firefly({minPixel: 1,maxPixel: 2,total: 20});
+	else $.firefly({minPixel: 1,maxPixel: 3,total: 40});
+}
+
+// This is for the click to copy
+/*let t;
+$(document).ready(()=>{
+	t = $(".ip").html();
+})
+$(document).on("click",".ip",()=>{
+	let copy = document.createElement("textarea");
+	copy.style.position = "absolute";
+	copy.style.left = "-99999px";
+	copy.style.top = "0";
+	copy.setAttribute("id", "ta");
+	document.body.appendChild(copy);
+	copy.textContent = t;
+	copy.select();
+	document.execCommand("copy");
+	$(".ip").html("<span class='extrapad'>Link copied!</span>");
+	setTimeout(function(){
+		$(".ip").html(t);
+		var copy = document.getElementById("ta");
+		copy.parentNode.removeChild(copy);
+	},800);
+});
+*/
